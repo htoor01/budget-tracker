26282a03e7095b50dcc052669cc522ad66446dca
Haaris Toor <haaristoor1@gmail.com>
2025-11-13 18:43:23 -0500

    basic skeleton

---
+++ script.js
@@ -0,0 +1,17 @@
+function totalIncome(incomeSources){
+    let resultIncome = 0;
+    resultIncome = resultIncome + incomeSources; // Placeholder logic
+    return resultIncome;
+};
+
+function totalExpenses(expenseItems){
+    let resultExpenses = 0;
+    resultExpenses = resultExpenses + expenseItems; // Placeholder logic
+    return resultExpenses;
+};
+
+let totalBalance = resultIncome - resultExpenses;
+
+function displayIncome(){};
+function displayExpenses(){};
+function displayBalance(){};
