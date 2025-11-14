d81462da949ea5884135a3c395b51f0c001840aa
Haaris Toor <haaristoor1@gmail.com>
2025-11-13 23:52:29 -0500

    restructure

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-function totalIncome(incomeSources){
-    let resultIncome = 0;
-    resultIncome = resultIncome + incomeSources; // Placeholder logic
-    return resultIncome;
+const userInput = document.getElementsById("entry-input"); //text from input field
+const addButton = document.getElementsById("add-entry-button"); //add entry to list
+const clearButton = document.getElementById("clear-entries-button"); //clear entry list
+const renderChoice = document.getElementById("render-choice"); //dropdown for render
+
+function renderList(){
+    if(renderChoice.value === 'option1'){
+        //render option 1
+        optionOne();
+    }else{
+        //render option 2
+        optionTwo();
+    };
 };
 
-function totalExpenses(expenseItems){
-    let resultExpenses = 0;
-    resultExpenses = resultExpenses + expenseItems; // Placeholder logic
-    return resultExpenses;
+function optionOne(){
+
+
 };
 
-let totalBalance = resultIncome - resultExpenses;
+function optionTwo(){
 
-function displayIncome(){};
-function displayExpenses(){};
-function displayBalance(){};
+
+};
+
+function storeData(){
+
+
+};
+
+function updateData(){
+
+
+};
